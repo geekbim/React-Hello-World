70356174709d1275f7d1d782f86388a2908bca9f
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-05-05 08:36:44 +0700

    React Hello World

---
+++ src/container/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
+
+class Home extends Component {
+    render() {
+        return (
+            <YouTubeComp />
+        )
+    }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './container/Home/Home';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
