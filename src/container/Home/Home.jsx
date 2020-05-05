5461ddf3a77fc27a761be892d6b48d172ab33357
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-05-05 08:50:15 +0700

    React Hello World

--- src/container/Home/Home.jsx
+++ src/container/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
+import '../../index.css';
 
 class Home extends Component {
     render() {
         return (
-            <YouTubeComp />
+            <div className="card-columns">
+                <YouTubeComp title="Card 1" desc="Desc 1" />
+                <YouTubeComp title="Card 2" desc="Desc 2" />
+                <YouTubeComp title="Card 3" desc="Desc 3" />
+                <YouTubeComp title="Card 4" desc="Desc 4" />
+                <YouTubeComp title="Card 5" desc="Desc 5" />
+            </div>
         )
     }
 }
